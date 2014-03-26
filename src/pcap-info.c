65c16d5be9ca49439db56c93dc718adc92c7d524
Nils O. Selåsdal <noselasd@fiane.dyndns.org>
2014-03-26 20:55:11 +0100

    Initial import

---
+++ src/pcap-info.c
@@ -0,0 +1,105 @@
+#include <pcap.h>
+#include <getopt.h>
+
+/* Small utility to show info and dump the hex content of 
+ * pcap files */
+
+static int count_packets;
+static int print_hex;
+
+static void usage(const char *progname)
+{
+    printf("Usage: %s [-hcp] file1.pcap file2.pcap ...", progname);
+    puts("\t-c count the number of packets");
+    puts("\t-p print a hex dump of each packet");
+    puts("\t-h this help");
+}
+
+static void dump_hex(const unsigned char *data, u_int len)
+{
+    u_int i;
+    for(i = 1; i <= len; i++) {
+        printf("%02X ", data[i - 1]);
+        if(i % 20 == 0)
+            putchar('\n');
+    }
+
+    if(i % 20 != 0)
+        putchar('\n');
+   putchar('\n');
+
+}
+
+static void pcap_info(const char *file)
+{
+    char errbuf[PCAP_ERRBUF_SIZE];
+    pcap_t *pcap;
+
+    pcap = pcap_open_offline(file, errbuf);
+    if(pcap == NULL) {
+        printf("Error opening %s: %s\n", file, errbuf);
+        return;
+    }
+
+    printf("%s : version_major %u version_minor %u link_type %s (%d decimal)\n", 
+            file, 
+            pcap_major_version(pcap),
+            pcap_minor_version(pcap),
+            pcap_datalink_val_to_name(pcap_datalink(pcap)),
+            pcap_datalink(pcap));
+            
+
+    if(count_packets || print_hex) {
+        unsigned int count = 0;
+        struct pcap_pkthdr hdr; 
+        const unsigned char *packet;
+
+        while((packet = pcap_next(pcap, &hdr)) != NULL) {
+            if(print_hex) {
+                printf("packet %u\n", count);
+                dump_hex(packet, hdr.len);
+            }
+
+            count++;
+        }
+
+        printf("Total packets: %u\n", count);
+
+    }
+
+    pcap_close(pcap);
+}
+
+int main(int argc, char *argv[])
+{
+    int c;
+
+    while((c = getopt(argc, argv, "cph")) != -1) {
+        switch(c) {
+
+            case 'c':
+                count_packets = 1;
+                break;
+
+            case 'p':
+                print_hex = 1;
+                break;
+
+            default: //fallthru
+                printf("unknown option %c\n", c);
+            case 'h':
+                usage(argv[0]);
+                return 1;
+            break;
+
+
+        }
+    }
+
+    while(optind < argc) {
+        pcap_info(argv[optind]);
+        optind++;
+    }
+
+    return 0;
+}
